31de7b34c63c2e57a30251722ad4a0e42b0b65f6
RajyalakshmiRodda <110736881+RajyalakshmiRodda@users.noreply.github.com>
2023-11-06 10:16:24 +0530

    Add files via upload

---
+++ Char_str.java
@@ -0,0 +1,8 @@
+public class Char_str {
+  public static void main(String[] args) {
+    char[] c={'r','a','j','y','a','l','a','k','s','h','m','m'};
+    String s=new String(c);
+    System.out.println("convert character to string");
+    System.out.println(s);
+  }
+}
